8c0064359d98bf0eee319810494d905a65d61398
Naman Chhibbar <n.chhibar@gmail.com>
2025-06-20 02:15:06 +0530

    Renamed app

--- ExerciseDetection/wsgi.py
+++ ExerciseDetection/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CountReps project.
+WSGI config for ExerciseDetection project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CountReps.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ExerciseDetection.settings')
 
 application = get_wsgi_application()
