91f19084771d22883c0f7d39ce6b79f53bcae8e2
Naman Chhibbar <n.chhibar@gmail.com>
2025-06-20 13:47:03 +0530

    Change app name to "App"

--- App/asgi.py
+++ App/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ExerciseDetection project.
+ASGI config for App project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ExerciseDetection.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'App.settings')
 
 application = get_asgi_application()
