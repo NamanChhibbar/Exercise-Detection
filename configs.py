94b7ebdc4fd89a962255c577005e98074d21cddf
Naman Chhibbar <n.chhibar@gmail.com>
2025-06-20 15:23:58 +0530

    Added configs.py to store pose landmarker and classifier configs

---
+++ configs.py
@@ -0,0 +1,7 @@
+# Mediapipe Pose Landmarker path and configuration
+POSE_LANDMARKER_PATH = 'pose_landmarker_full.task'
+POSE_LANDMARKER_FRAME_RATE = 3
+POSE_LANDMARKER_MAX_FRAMES = 100
+
+# Landmark Classifier path
+LANDMARK_CLASSIFIER_PATH = 'classifier.keras'
