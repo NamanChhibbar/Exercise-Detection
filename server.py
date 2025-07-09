4b4587595ff15a2a5965d0b446f1937306905276
Naman Chhibbar <n.chhibar@gmail.com>
2025-07-09 15:12:00 +0530

    Updated server script to use normalized_landmarks as model inputs and angles for repetition calculation

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from botocore.exceptions import ClientError
 from dotenv import load_dotenv
 
 from src.video_utils import video_capture_from_bytes, S3Connection
-from src.pose_utils import LandmarkExtractor, convert_to_cosine_angles
+from src.pose_utils import LandmarkExtractor, normalize_landmarks, convert_to_cosine_angles
 from src.classifier import SequenceClassifier
 from src.repetitions_utils import max_variance_series, count_cycles
 from configs import (
@@ -41,28 +41,11 @@ classifier = SequenceClassifier.load_model(model_path=LANDMARK_CLASSIFIER_PATH)
 
 @app.post('/detect-exercise/')
 async def detect_exercise(body: DetectExerciseBody):
+  bucket = body.bucket
+  key = body.key
+  # Fetch video bytes from S3
   try:
-    bucket = body.bucket
-    key = body.key
-    # Fetch video bytes from S3
     video_bytes = s3_connection.fetch_video(bucket, key)
-    if not video_bytes:
-      raise HTTPException(status_code=404, detail='Video file not found')
-    # Get video capture object and temporary file name
-    cap, tmp_name = video_capture_from_bytes(video_bytes)
-    # Extract landmarks from the video
-    landmarks = extractor.extract(cap)
-    # Release the video capture object
-    cap.release()
-    # Clean up the temporary file
-    os.remove(tmp_name)
-    # Get cosine angles from landmarks
-    angles = convert_to_cosine_angles(landmarks)
-    # Normalize landmarks
-    normalized_landmarks = 
-    tensor_input = tf.convert_to_tensor(landmarks, dtype=tf.float32)[tf.newaxis, ...]
-    predicted_class = classifier.predict(tensor_input)[0]
-    return JSONResponse(content={'predicted_class': predicted_class}, status_code=200)
   # Handle S3 errors
   except ClientError as e:
     match e.response['Error']['Code']:
@@ -77,6 +60,34 @@ async def detect_exercise(body: DetectExerciseBody):
           status_code=500,
           detail=f'Error {str(e)} of type {type(e).__module__}.{type(e).__name__} occurred while accessing S3'
         )
+  # If no video bytes are returned, raise a 404 error
+  if not video_bytes:
+    raise HTTPException(status_code=404, detail='No video data found')
+  try:
+    # Get video capture object and temporary file name
+    cap, tmp_name = video_capture_from_bytes(video_bytes)
+    # Extract landmarks from the video
+    landmarks = extractor.extract(cap)
+    # Release the video capture object
+    cap.release()
+    # Clean up the temporary file
+    os.remove(tmp_name)
+    # Get cosine angles from landmarks to count repetitions
+    angles = convert_to_cosine_angles(landmarks)
+    # Normalize and flatten landmarks
+    landmarks = normalize_landmarks(landmarks).resize(-1, 96)
+    # Create tensor input of shape (batch_size, sequence_length, num_features)
+    tensor_input = tf.convert_to_tensor(landmarks, dtype=tf.float32)[tf.newaxis, ...]
+    # Predict class using
+    predicted_class = classifier.predict(tensor_input)[0]
+    # Get max variance series
+    series = max_variance_series(angles)
+    # Count repetitions in the series
+    repetitions = count_cycles(series, frac=0.1)
+    return JSONResponse(
+      content={'predicted_class': predicted_class, 'repetitions': repetitions},
+      status_code=200
+    )
   # General error handling
   except Exception as e:
     raise HTTPException(
@@ -86,25 +97,33 @@ async def detect_exercise(body: DetectExerciseBody):
 
 @app.post('/detect-exercise-local/')
 async def detect_exercise(body: DetectExerciseLocalBody):
+  file_location = body.file_location
   try:
-    file_location = body.file_location
-    # Process video and make prediction
-    cap = cv2.VideoCapture(file_location)
+    # Get video capture object and temporary file name
+    cap =cv2.VideoCapture(file_location)
+    # Extract landmarks from the video
     landmarks = extractor.extract(cap)
+    # Release the video capture object
+    cap.release()
+    # Get cosine angles from landmarks to count repetitions
     angles = convert_to_cosine_angles(landmarks)
-    tensor_input = tf.convert_to_tensor(angles, dtype=tf.float32)[tf.newaxis, ...]
+    # Normalize and flatten landmarks
+    landmarks = normalize_landmarks(landmarks).resize(-1, 96)
+    # Create tensor input of shape (batch_size, sequence_length, num_features)
+    tensor_input = tf.convert_to_tensor(landmarks, dtype=tf.float32)[tf.newaxis, ...]
+    # Predict class using
     predicted_class = classifier.predict(tensor_input)[0]
-    # Get max variance series and count repetitions
+    # Get max variance series
     series = max_variance_series(angles)
+    # Count repetitions in the series
     repetitions = count_cycles(series, frac=0.1)
     return JSONResponse(
       content={'predicted_class': predicted_class, 'repetitions': repetitions},
       status_code=200
     )
-  except KeyError as e:
-    raise HTTPException(status_code=400, detail=f'Missing key: {e}')
+  # General error handling
   except Exception as e:
-    return JSONResponse(
-      content={'error': str(e), 'type': f'{type(e).__module__}.{type(e).__name__}'},
-      status_code=500
+    raise HTTPException(
+      status_code=500,
+      detail=f'Error {str(e)} of type {type(e).__module__}.{type(e).__name__} occurred'
     )
