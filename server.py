e7b43645f62bd9d67824d3572df99e6f58ef31e2
Naman Chhibbar <n.chhibar@gmail.com>
2025-07-09 14:06:06 +0530

    Added PyDantic data modelling

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-import json
-
 import cv2
 import tensorflow as tf
-from fastapi import FastAPI, Request, HTTPException
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from botocore.exceptions import ClientError
 from dotenv import load_dotenv
@@ -16,7 +15,17 @@ from configs import (
   LANDMARK_CLASSIFIER_PATH
 )
 
+class DetectExerciseBody(BaseModel):
+  '''Request body format for detect-exercise endpoint.'''
+  bucket: str
+  key: str
+
+class DetectExerciseLocalBody(BaseModel):
+  file_location: str
+
+# Load environment variables
 load_dotenv()
+# Initialize FastAPI app
 app = FastAPI()
 
 s3_connection = None
@@ -29,14 +38,10 @@ extractor = LandmarkExtractor(
 classifier = SequenceClassifier.load_model(model_path=LANDMARK_CLASSIFIER_PATH)
 
 @app.post('/detect-exercise/')
-async def detect_exercise(request: Request):
+async def detect_exercise(body: DetectExerciseBody):
   try:
-    body = await request.body()
-    data = json.loads(body)
-    if not isinstance(data, dict):
-      raise HTTPException(status_code=400, detail='Invalid JSON')
-    bucket = data['bucket']
-    key = data['key']
+    bucket = body.bucket
+    key = body.key
     # Fetch video bytes from S3
     video_bytes = s3_connection.fetch_video(bucket, key)
     if not video_bytes:
@@ -48,12 +53,6 @@ async def detect_exercise(request: Request):
     tensor_input = tf.convert_to_tensor(landmarks, dtype=tf.float32)[tf.newaxis, ...]
     predicted_class = classifier.predict(tensor_input)[0]
     return JSONResponse(content={'predicted_class': predicted_class}, status_code=200)
-  # Handle JSON decoding errors
-  except json.JSONDecodeError:
-    raise HTTPException(status_code=400, detail='Invalid JSON')
-  # Handle missing keys in the request
-  except KeyError as e:
-    raise HTTPException(status_code=400, detail=f'Missing key: {e}')
   # Handle S3 errors
   except ClientError as e:
     match e.response['Error']['Code']:
@@ -64,24 +63,21 @@ async def detect_exercise(request: Request):
       case 'AccessDenied':
         raise HTTPException(status_code=403, detail='Access denied to the bucket or key')
       case _:
-        return JSONResponse(
-          content={'error': str(e), 'type': f'{type(e).__module__}.{type(e).__name__}'},
-          status_code=500
+        raise HTTPException(
+          status_code=500,
+          detail=f'Error {str(e)} of type {type(e).__module__}.{type(e).__name__} occurred while accessing S3'
         )
+  # General error handling
   except Exception as e:
-    return JSONResponse(
-      content={'error': str(e), 'type': f'{type(e).__module__}.{type(e).__name__}'},
-      status_code=500
+    raise HTTPException(
+      status_code=500,
+      detail=f'Error {str(e)} of type {type(e).__module__}.{type(e).__name__} occurred'
     )
 
 @app.post('/detect-exercise-local/')
-async def detect_exercise(request: Request):
+async def detect_exercise(body: DetectExerciseLocalBody):
   try:
-    body = await request.body()
-    data = json.loads(body)
-    if not isinstance(data, dict):
-      raise HTTPException(status_code=400, detail='Invalid JSON')
-    file_location = data['file_location']
+    file_location = body.file_location
     # Process video and make prediction
     cap = cv2.VideoCapture(file_location)
     landmarks = extractor.extract(cap)
@@ -95,8 +91,6 @@ async def detect_exercise(request: Request):
       content={'predicted_class': predicted_class, 'repetitions': repetitions},
       status_code=200
     )
-  except json.JSONDecodeError:
-    raise HTTPException(status_code=400, detail='Invalid JSON')
   except KeyError as e:
     raise HTTPException(status_code=400, detail=f'Missing key: {e}')
   except Exception as e:
